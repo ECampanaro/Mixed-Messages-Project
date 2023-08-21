4f708fc0e21df8d9d6e7f4ace6d642c91fd3b12d
ECampanaro <emilycampanaro@gmail.com>
2023-08-21 10:53:33 -0400

    change to fortuneEnds object verbiage and add selectRandom function

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const fortuneStarters = ['You', 'Your', 'The'];
-const fortuneMiddles = [['are', 'will', 'should'], ['future', 'happiness', 'life'], ['world', 'universe', 'sun']];
+
+const fortuneMiddles = {
+    you: ['are', 'will', 'should'], 
+    your: ['future', 'happiness', 'life'], 
+    the: ['world', 'universe', 'sun'],
+};
+
 const fortuneEnds = {
-    'You are': ['having a good day', 'experiencing good fortune', 'learning a valuable lesson'],
-    'You will': ['meet someone special', 'receive a large sum of money', 'be very happy'],
-    'You should': ['be careful crossing the street', 'never repeat a secret told in confidence', 'trust yourself'], 
-   'Your future': ['is bright', 'holds many possibilities', 'is an unknown'], 
-    'Your happiness': ['is certain', 'is just around the corner', 'will last forever'], 
-    'Your life': ['is just beginning', 'will be full of meaningful relationships', 'will begin when you want it to'], 
-    'The universe': ['wants you to succeed', 'is on your side', 'holds many mysteries'], 
-    'The sun': ['is always shining, we just don/t always see it', 'will come out tomorrow', 'creates light and shadows'], 
-    'The world': ['is your oyster', 'demands to be seen', 'is a beautiful place']
+    'you are': ['having a good day', 'experiencing good fortune', 'learning a valuable lesson'],
+    'you will': ['meet someone special', 'receive a large sum of money', 'be very happy'],
+    'you should': ['be careful crossing the street', 'never repeat a secret told in confidence', 'trust yourself'], 
+   'your future': ['is bright', 'holds many possibilities', 'is an unknown'], 
+    'your happiness': ['is certain', 'is just around the corner', 'will last forever'], 
+    'your life': ['is just beginning', 'will be full of meaningful relationships', 'will begin when you want it to'], 
+    'the universe': ['wants you to succeed', 'is on your side', 'holds many mysteries'], 
+    'the sun': ['is always shining, we just don/t always see it', 'will come out tomorrow', 'creates light and shadows'], 
+    'the  world': ['is your oyster', 'demands to be seen', 'is a beautiful place']
 }
 
-console.log(fortuneEnds['The world']);
+//function to randomly select an item from an array
+const selectRandom = (input) => {
+    return input[Math.floor(Math.random() * input.length)];
+};
+
+
+
