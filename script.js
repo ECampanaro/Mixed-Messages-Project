29c7f662bbbdf014d6fdcf8c2d800d68686a8006
ECampanaro <emilycampanaro@gmail.com>
2023-08-21 10:38:00 -0400

    first commit, add script.js with fortunes

---
+++ script.js
@@ -0,0 +1,15 @@
+const fortuneStarters = ['You', 'Your', 'The'];
+const fortuneMiddles = [['are', 'will', 'should'], ['future', 'happiness', 'life'], ['world', 'universe', 'sun']];
+const fortuneEnds = {
+    'You are': ['having a good day', 'experiencing good fortune', 'learning a valuable lesson'],
+    'You will': ['meet someone special', 'receive a large sum of money', 'be very happy'],
+    'You should': ['be careful crossing the street', 'never repeat a secret told in confidence', 'trust yourself'], 
+   'Your future': ['is bright', 'holds many possibilities', 'is an unknown'], 
+    'Your happiness': ['is certain', 'is just around the corner', 'will last forever'], 
+    'Your life': ['is just beginning', 'will be full of meaningful relationships', 'will begin when you want it to'], 
+    'The universe': ['wants you to succeed', 'is on your side', 'holds many mysteries'], 
+    'The sun': ['is always shining, we just don/t always see it', 'will come out tomorrow', 'creates light and shadows'], 
+    'The world': ['is your oyster', 'demands to be seen', 'is a beautiful place']
+}
+
+console.log(fortuneEnds['The world']);
